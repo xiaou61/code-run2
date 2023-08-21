e28a90dddf844dca5855b222224f0f5b74b04de8
xiaou <3153566913@qq.com>
2023-08-22 07:56:11 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = {
-    publicPath: '/code-run/',
+    publicPath: '',
     outputDir: './docs/',
     lintOnSave: false,
     productionSourceMap: false,
